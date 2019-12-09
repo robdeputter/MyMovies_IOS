31e93cd957f6d48c68261514ea317e404a24743d
robdeputter <robdeputter@hotmail.com>
2019-12-09 18:34:59 +0100

    First commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Rob De Putter on 09/12/2019.
+//  Copyright © 2019 Rob De Putter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
