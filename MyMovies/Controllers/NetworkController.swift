53fbedef8521e92804deab4aaaa79a9b77451c3d
robdeputter <robdeputter@hotmail.com>
2019-12-12 23:10:32 +0100

    Add fetchImage, fetchMovieSerieDetail and fetchNewReleases

--- MyMovies/Controllers/NetworkController.swift
+++ MyMovies/Controllers/NetworkController.swift
@@ -7,14 +7,16 @@
  //
  
  import Foundation
+ import UIKit
  
  class NetworkController{
     static let instance : NetworkController = NetworkController()
     
     let baseURLimdb = URL(string: "https://movie-database-imdb-alternative.p.rapidapi.com")!
+    let baseURLunogs = URL(string: "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi/")!
     
-    func fetchSearchMovieSerie(with name : String?, with year : String? , with type : String?,
-                               completion : @escaping (_ movieSeries : [MovieSerie]?) -> Void){
+    func fetchSearchMovieSeries(with name : String?, with year : String? , with type : String?,
+                                completion : @escaping (_ movieSeries : [MovieSerie]?) -> Void){
         
         let queries : [String : String] = [
             "s" : name ?? "",
@@ -36,11 +38,66 @@
             }
         }
         task.resume()
-        
-        
     }
     
+    func fetchMovieSerieDetail(with imdbId : String, completion : @escaping (_ movieSerieDetail : MovieSerieDetail?) -> Void){
+        let query : [String : String] = [
+            "i" : imdbId
+        ]
+        
+        let url = baseURLimdb.withQueries(query)!
+        let req = url.addUrlHeaders(for: url)
+        
+        let task = URLSession.shared.dataTask(with: req){
+            (data, response, error) in
+            let jsonDecoder = JSONDecoder()
+            if let data = data, let movieSerieDetail = try? jsonDecoder.decode(MovieSerieDetail.self, from: data){
+                completion(movieSerieDetail)
+            }
+            else{
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
     
+    func fetchImage(with url : URL, completion : @escaping (UIImage?) -> Void){
+        let task = URLSession.shared.dataTask(with: url){
+            (data, response, error) in
+            if let data = data{
+                URLCache.shared.storeCachedResponse(CachedURLResponse(response: response!, data: data), for: URLRequest(url: url))
+                
+                let img = UIImage(data: data)
+                completion(img)
+            }
+            else{
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+    
+    func fetchNewReleases(completion : @escaping (_ newReleases : [NewRelease]?) -> Void){
+        let queries : [String : String] = [
+            "q": "get:new7:BE",
+            "p": "1"
+        ]
+        
+        let url = baseURLimdb.withQueries(queries)!
+        let req = url.addUrlHeaders(for: url)
+        
+        let task = URLSession.shared.dataTask(with: req){
+            (data,response, error) in
+            let jsonDecoder = JSONDecoder()
+            if let data = data, let newReleaseResponse = try? jsonDecoder.decode(NewReleaseResponse.self, from: data){
+                completion(newReleaseResponse.items)
+            }
+            else{
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
     
  }
  extension URL{
